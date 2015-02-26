bc2b7d91498445158a5c136b059e592cdbd51bb5
Josh Owens <joshua.owens@gmail.com>
2015-02-26 10:30:58 -0800

    Start with ingredients and autoform.

--- client/templates/drinks/drink_item.js
+++ client/templates/drinks/drink_item.js
@@ -1,1 +1,23 @@
-Template.drinkItem.helpers({  ownDrink: function() {    return this.userId == Meteor.userId();  },  upvotedClass: function() {    var userId = Meteor.userId();    if (userId && !_.include(this.upvoters, userId)) {      return 'btn-success upvotable';    } else {      return 'disabled';    }  }});Template.drinkItem.events({  'click .upvotable': function(e) {    e.preventDefault();    Meteor.call('upvote', this._id);  }});
+Template.drinkItem.helpers({
+  ingredients: function() {
+    return Ingredients.find();
+  },
+  ownDrink: function() {
+    return this.userId == Meteor.userId();
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-success upvotable';
+    } else {
+      return 'disabled';
+    }
+  }
+});
+
+Template.drinkItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,34 @@
-Meteor.publish('drinks', function(options) {  check(options, {    sort: Object,    limit: Number  });  return Drinks.find({}, options);});Meteor.publish('singleDrink', function(id) {  check(id, String);  return Drinks.find(id);});Meteor.publish('ingredients', function(ingredientId) {  return Ingredients.find();})Meteor.publish('comments', function(drinkId) {  check(drinkId, String);  return Comments.find();});Meteor.publish('notifications', function() {  return Notifications.find({userId: this.userId, read: false});});
+Meteor.publish('drinks', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Drinks.find({}, options);
+});
+
+Meteor.publish('singleDrink', function(id) {
+  check(id, String);
+  var ingredients = null;
+  drinks = Drinks.find(id);
+  drink = drinks.fetch()[0];
+  if (drink) {
+    ingredientIds = _(drink.ingredients).map(function(ingredient) {
+      return ingredient.id;
+    });
+    ingredients = Ingredients.find({_id: {$in: ingredientIds}});
+  }
+  return [drinks, ingredients];
+});
+
+Meteor.publish('ingredients', function(ingredientId) {
+  return Ingredients.find();
+});
+
+Meteor.publish('comments', function(drinkId) {
+  check(drinkId, String);
+  return Comments.find();
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
+});
